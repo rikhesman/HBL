eaa28c60d50416f1f16b26fd33fdf79af4d24895
rikhesman <rik.heesink@windesheim.nl>
2016-12-06 20:39:25 +0100

    build head gemaakt
    Head include gemaakt en in build gezet

--- contact.php
+++ contact.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title>De Bijlesjuf</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="./css/bootstrap.css">
-  <link rel="stylesheet" href="./css/bootstrap1.css">
-     <link rel="shortcut icon" href="./foto/logosonnega.ico">
-  <script src="./js/javascript.js"></script>
-  <script src="./js/javascript1.js"></script>
-  <script src='https://www.google.com/recaptcha/api.js'></script>
+  <?php include('build/head.php');
+    session_start();?>
 </head>
 <body>
 
