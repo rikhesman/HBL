4983dc2a08205c57f2ab78b7780c964c57d38f18
TristanHoff <tristanhoff@hotmail.com>
2016-12-09 11:17:37 +0100

    formulier
    Formulier aangepast zodat het past bij de database

--- includes/model/dataAccountManagement.php
+++ includes/model/dataAccountManagement.php
@@ -6,21 +6,21 @@
 class dataAccountManagement extends connection {
 	
 	// Registreren
-	public function setRegister($nameP1, $nameP2, $email,$tel,$fName,$lName,$bDate,$school,$class,$dys,$comment) {
-		$sql = "INSERT INTO users (namep1, namep2, email, telephone, fname, lname, bdate, school, class, dys, comment) 
-		VALUES (:namep1,:namep2, :email, :telephone, :fname, :lname, :bdate, :school, :class, :dys, :comment);"; 		
+	public function setRegister($username, $password, $fname,$insertion,$lname,$rol,$email,$tel,$dys,$comment,$date) {
+		$sql = "INSERT INTO users (username, password, f_name, insertioon, l_name, rol, email, tel, dys, comment, join_date) 
+		VALUES (:username,:password, :f_name, :insertion, :l_name, :rol, :email, :tel, :dys, :comment, :join_date);"; 		
 		$q = $this -> conn -> prepare($sql);
-		$q -> bindValue(':namep1', $nameP1, PDO::PARAM_STR);		
-		$q -> bindValue(':namep2', $nameP2, PDO::PARAM_STR);		
-		$q -> bindValue(':email', $email, PDO::PARAM_STR);
-		$q -> bindValue(':telephone', $tel, PDO::PARAM_STR);	
-		$q -> bindValue(':fname', $fName, PDO::PARAM_STR);	
-		$q -> bindValue(':lname', $lName, PDO::PARAM_STR);	
-		$q -> bindValue(':bdate', $bDate, PDO::PARAM_STR);	
-		$q -> bindValue(':school', $school, PDO::PARAM_STR);	
-		$q -> bindValue(':class', $class, PDO::PARAM_STR);	
+		$q -> bindValue(':username', $username, PDO::PARAM_STR);		
+		$q -> bindValue(':password', $password, PDO::PARAM_STR);		
+		$q -> bindValue(':f_name', $fname, PDO::PARAM_STR);
+		$q -> bindValue(':insertion', $insertion, PDO::PARAM_STR);	
+		$q -> bindValue(':l_name', $lname, PDO::PARAM_STR);	
+		$q -> bindValue(':rol', $rol, PDO::PARAM_STR);	
+		$q -> bindValue(':email', $email, PDO::PARAM_STR);	
+		$q -> bindValue(':tel', $tel, PDO::PARAM_STR);	
 		$q -> bindValue(':dys', $dys, PDO::PARAM_STR);	
-		$q -> bindValue(':comment', $comment, PDO::PARAM_STR);				
+		$q -> bindValue(':comment', $comment, PDO::PARAM_STR);	
+		$q -> bindValue(':join_date', $date, PDO::PARAM_STR);				
 		$q -> execute();
 		
 		return true;
