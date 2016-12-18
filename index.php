bf72604a24ebed3dc52dc3f8ed199e82d2609a6a
Thijs van der Veen <thijs.v.d.veen@live.nl>
2016-12-18 19:50:10 +0100

    updating

--- index.php
+++ index.php
@@ -2,37 +2,69 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <?php include('build/head.php');?>
+        <?php include('build/head.php'); ?>
     </head>
     <body>
-        <?php include('build/navbar.php'); ?>
+        <?php include('build/navbar.php');
+        //$screenWidth = ?> <script type = "text/javascript">document.getElementById("test").innerHTML= 'screen.width'</script>
+        <?php //print ('<style>@media (max-width:768 px) and (min-width:384px){
+                //.carousel.slide{
+                //  margin-left: 50px;
+                //}
+            //}
+//</style>');
+        ?>
+
         <div id="myCarousel" class="carousel slide" data-ride="carousel">
             <!-- Indicators -->
-            <ol class="carousel-indicators">
-                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                <li data-target="#myCarousel" data-slide-to="1"></li>
-                <li data-target="#myCarousel" data-slide-to="2"></li>
-            </ol>
+
+
             <!-- Wrapper for slides -->
             <div class="carousel-inner" >
-                <div class="item active">
-                    <img class="img-responsive center-block" src="./foto/Slideshow/foto1.jpg" alt="Image">        
-                </div>
-                <div class="item">
-                    <img class="img-responsive center-block" src="./foto/Slideshow/foto2.jpg" alt="Image">
-                </div>
-                <div class="item">
-                    <img class="img-responsive center-block" src="./foto/Slideshow/foto3.jpg" alt="Image">
-                </div>
+                <?php
+                $dir = 'foto\Slideshow/';
+                $count = true;
+                $total = 0;
+                $dir_contents = glob($dir . "*.jpg");
+                $countSlide = TRUE;
+                //hierboven staan de variabelen voor foto's en de indicators, hieronder de functies
+                foreach ($dir_contents as $file) {
+                    ?>
+                    <div class="item <?php
+                    if ($count == true) {
+                        echo 'active';
+                        $count = false;
+                    }
+                    ?>"><img class="img-responsive center-block" src="<?php echo $file; ?>" alt="error" /></div>
+                         <?php
+                         $total ++;
+                     }
+                     /* hier maak ik de indicators aan per foto */
+                     ?>
             </div>
+            <ol class="carousel-indicators">
+                <?php
+                for ($i = 0; $i < $total; $i++) {
+                    ?>
+                    <li data-target="#myCarousel" data-slide-to="<?php echo $i; ?>" <?php if ($countSlide == true) {
+                        ?>
+                            class="active"
+                            <?php
+                            $countSlide = false;
+                        }
+                        ?>></li>
+    <?php
+}
+?>
+            </ol>
             <!-- Left and right controls -->
             <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
+                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
             </a>
             <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
+                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
             </a>
         </div>
         <div class="row">
@@ -72,7 +104,7 @@
             </div>
             <div class="col-md-4">
                 <!--Rechter kant-->
-            </div>
+        </div>
         </div>
     </body>
 </html>
--- review.php
+++ review.php
@@ -4,32 +4,58 @@ include('includes/autoloader.php');
 
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <?php include('build/head.php');?>
-    </head>
-    <body>
-        <?php include('build/navbar.php'); ?>
-        <div class="row">
-            <div class="col-md-4"><!--Linker kant--></div>
-            <div class="col-md-4">
-                <form method="post" accept-charset="utf-8">
-                    <h3>Schrijf een review over &ldquo;De Bijlesjuf&rdquo;</h3>
-                    <!-- Rating -->
-                    <p>
-                        <label for="rating">Waardering</label>
-                        <input type="radio" name="rating" value="1" /> 1 
-                        <input type="radio" name="rating" value="2" /> 2
-                        <input type="radio" name="rating" value="3" /> 3 
-                        <input type="radio" name="rating" value="4" /> 4 
-                        <input type="radio" name="rating" value="5" /> 5
-                    </p>
-                    <label for="review">Review</label>
-                    <textarea name="review" placeholder="Geef ons uw mening" rows="8" cols="40"></textarea>
-                    <p>
-                    <input type="submit" value="Plaats Review" name="save_review">
-                </form>
-                <div class="col-md-4"><!--Rechter kant--></div>
-            </div>
-        </div>
-    </body>
+<head>
+   <?php include('build/head.php');?>
+</head>
+<body>
+<?php include('build/navbar.php'); ?>
+
+   <div class="row"> 
+  <div class="col-md-4"><!--Linker kant--></div>
+    <div class="col-md-4">
+
+
+        
+        
+
+
+<form method="post" accept-charset="utf-8">
+        <?php if($_SESSION['user']['role'] == 'Ouder' ){        
+      	 echo '         
+    	<h3>Schrijf hier uw review</h3>	
+    	<!-- Rating -->
+    <p>
+    	<label for="rating">Rating</label>
+    	<input type="radio" name="rating" value="1" /> 1 
+      	<input type="radio" name="rating" value="2" /> 2
+      	<input type="radio" name="rating" value="3" /> 3 
+      	<input type="radio" name="rating" value="4" /> 4 
+      	<input type="radio" name="rating" value="5" /> 5
+    </p>
+    
+    
+    	<label for="review">Review</label>
+    	<textarea name="review" placeholder="Geef ons uw mening" rows="8" cols="40"></textarea>	
+    	<input type="submit" value="Plaats Review" name="save_review">
+	
+</form>
+  ';}
+        ?>
+        
+<?php
+		 foreach (reviewMAnagement::getReview() as $review) {
+			echo'
+			<div class="review_block">
+				<h2>Gebruiker: '.$review['username'].'</h2>
+				<p>Rating: '.$review['rating'].'/5</p>
+				<p>Review:<br>'.$review['review'].'</p>
+			</div>';
+		}
+		?>
+
+    <div class="col-md-4"><!--Rechter kant--></div> 
+    </div>
+
+
+</body>
 </html>
