2cc43a161ef1c4b7218edfa264b34222987a2a9a
rikhesman <rik.heesink@windesheim.nl>
2016-12-21 13:24:10 +0100

    form.php
    form.php

--- question.php
+++ question.php
@@ -19,7 +19,7 @@
                     <div class="form-group">Onderwerp:  <span class="error">*</span><input type="text" class="form-control" placeholder="Vul hier in" name="subject" value="<?php echo (isset($_POST['subject']) ? $_POST['subject'] : '') ?>"></div>
                     <div class="form-group">Bericht:  <span class="error">*</span><textarea rows="5" class="form-control" placeholder="Vul hier in" cols="22" name="content"><?php echo (isset($_POST['comment']) ? $_POST['content'] : '') ?></textarea></div>
                     <br>
-                    <div class="form-group"><input type="submit" name="toquestion" value="Verstuur vraag"></div>
+                    <div class="form-group"><input class="btn btn-primary" type="submit" name="toquestion" value="Verstuur vraag"></div>
                 </form>
             </div>
             <div class="col-md-4"><!--Rechter kant--></div> 
